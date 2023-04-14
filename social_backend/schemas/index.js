bd94940d0fc326f4ddf6cffc63c6182d2162c860
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-14 19:10:26 +0400

    Succesfuly added schemas for sanity

---
+++ social_backend/schemas/index.js
@@ -0,0 +1,8 @@
+// schemas/index.js
+import pin from './pin';
+import user from './user';
+import comment from './comment';
+import postedBy from './postedBy';
+import save from './save';
+
+export const schemaTypes = [user, pin, comment, postedBy, save];
