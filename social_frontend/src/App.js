45b71baf9fb33bfdb5b77a8e4329a3ec68cf6149
FIRST_NAME LAST_NAME <nikagermanishvili5@gmail.com>
2023-04-14 19:49:47 +0400

    tailwind succesfully added

---
+++ social_frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello my bRADA</h1>
+      <h2>hello</h2>
+    </>
+  )
+}
+
+export default App
